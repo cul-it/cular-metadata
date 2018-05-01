92eac6d64a3777b0271cc88c7d2169056830eab5
Dianne Dietrich <dd388@cornell.edu>
2018-05-01 13:33:09 -0400

    mismatch bug fixed, I hope

--- hashdeep_to_json.py
+++ hashdeep_to_json.py
@@ -35,27 +35,28 @@ def main():
 
     reader = csv.DictReader(args.inputfile)
     for row in reader:
-        hashlist[row['filename']] = (row['md5'], row['sha1'])
+        hashlist[row['filename']] = (row['size'], row['sha1'])
         # figure out a more elegant solution to this later
         dirs.add(os.path.split(row['filename'])[0])
         files.add(os.path.split(row['filename'])[1])
+
 
     items = defaultdict(list)
     for d in dirs:
         for f in files:
             if len(hashlist['{0}/{1}'.format(d,f)]) == 0:
-                items[d].append({f: {'sha1': '', 'md5': '', 'size': ''}})
+                pass # File isn't in fixity supplied fixity...!
             else:
-                thismd5 = hashlist['{0}/{1}'.format(d,f)][0]
+                thissize = hashlist['{0}/{1}'.format(d,f)][0]
                 thissha1 = hashlist['{0}/{1}'.format(d,f)][1]
-                items[d].append({f: {'sha1': thissha1, 'md5': thismd5, 'size': ''}})
+                items[d].append({f: {'sha1': thissha1, 'size': thissize}})
 
     em = {args.depcoll: {'phys_coll_id': args.physcollid,
                          'steward': args.steward,
                          'items': items}}
 
     sdepcoll = args.depcoll.replace('/', '_')
-    jsonfilename = '_EM_{0}.json'.format(sdepcoll.upper())
+    jsonfilename = '_EM_{0}.json'.format(sdepcoll)
 
     with open(jsonfilename, 'w') as output:
         output.write(json.dumps(em, indent=4))
